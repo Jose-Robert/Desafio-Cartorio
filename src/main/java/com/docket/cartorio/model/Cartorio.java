e9472edf6168dbd73e71b4df16b64c657ce92873
José Robert <jose.goncalves@conductor.com.br>
2020-01-29 13:06:30 -0300

    refatorando codigo

--- src/main/java/com/docket/cartorio/model/Cartorio.java
+++ src/main/java/com/docket/cartorio/model/Cartorio.java
@@ -7,7 +7,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -15,16 +17,18 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 
-@Entity
 @Getter
 @Setter
 @EqualsAndHashCode
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "CARTORIO")
 public class Cartorio implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	@ApiModelProperty("Identificador do cartorio")
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
